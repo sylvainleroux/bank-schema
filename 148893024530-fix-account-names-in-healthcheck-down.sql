ed1ec67796799544150b033bc6d161dc6457aa56
Sylvain Le Roux <sylvain.leroux@gmail.com>
2017-03-08 00:45:55 +0100

    Fix account names

---
+++ 148893024530-fix-account-names-in-healthcheck-down.sql
@@ -0,0 +1,6 @@
+-- direction: down
+-- backref: 148892899340
+-- ref: 148893024530
+
+
+
---
+++ 148893024530-fix-account-names-in-healthcheck-up.sql
@@ -0,0 +1,22 @@
+-- direction: up
+-- backref: 148892899340
+-- ref: 148893024530
+
+USE `bank`;
+
+CREATE
+     OR REPLACE ALGORITHM = UNDEFINED
+    DEFINER = `sleroux`@`%`
+    SQL SECURITY DEFINER
+VIEW `bank`.`ops` AS
+    SELECT
+        `bank`.`operation`.`year` AS `year`,
+        `bank`.`operation`.`month` AS `month`,
+        `bank`.`operation`.`catego` AS `catego`,
+        SUM(`bank`.`operation`.`credit`) AS `credit`,
+        SUM(`bank`.`operation`.`debit`) AS `debit`
+    FROM
+        `bank`.`operation`
+    WHERE
+        (`bank`.`operation`.`compte` IN (_LATIN1'CMB.COMPTE_CHEQUE' , _LATIN1'BPO.COMPTE_CHEQUE'))
+    GROUP BY `bank`.`operation`.`year` , `bank`.`operation`.`month` , `bank`.`operation`.`catego`;
