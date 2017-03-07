4abacaa9aa304c564115004cbb99da3b1718850e
Sylvain Le Roux <sylvain.leroux@gmail.com>
2017-03-08 00:14:39 +0100

    update account length

---
+++ 148892836750-account-name-down.sql
@@ -0,0 +1,7 @@
+-- direction: down
+-- backref: 147008419470
+-- ref: 148892836750
+
+
+ALTER TABLE `bank`.`budget`
+CHANGE COLUMN `compte` `compte` VARCHAR(20) NULL DEFAULT NULL ;
---
+++ 148892836750-account-name-up.sql
@@ -0,0 +1,7 @@
+-- direction: up
+-- backref: 147008419470
+-- ref: 148892836750
+
+
+ALTER TABLE `bank`.`budget` 
+CHANGE COLUMN `compte` `compte` VARCHAR(10) NULL DEFAULT NULL ;
